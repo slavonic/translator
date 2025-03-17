641d8268f1a1ebd3c0906569f8d9537c59d7c90d
Mike Kroutikov <pgmmpk@gmail.com>
2025-03-17 15:06:24 -0400

    addentum mechanism for accent engine

--- data/accent.py
+++ data/accent.py
@@ -1,7 +1,11 @@
 import collections
 
 
-def accent(fname='cu-words-civic-dedup.txt', outname='cu-words-civic-dedup-accent.txt'):
+def accent(
+    fname='cu-words-civic-dedup.txt',
+    outname='cu-words-civic-dedup-accent.txt',
+    addendum='accent-addendum.txt'
+):
     with open(fname, encoding='utf-8') as f:
         data = [l.strip().split() for l in f]
     print(f'Loaded {len(data)} samples')
@@ -9,6 +13,19 @@ def accent(fname='cu-words-civic-dedup.txt', outname='cu-words-civic-dedup-accen
     byru = collections.defaultdict(collections.Counter)
     for _, ru, count in data:
         byru[ru.replace('\u0301', '')].update([ru] * int(count))
+
+    with open(addendum, encoding='utf-8') as f:
+        addendum = [l.strip().split('|') for l in f]
+    print(f'..and {len(addendum)} addenda')
+    for _, w in addendum:
+        index = w.index('+')
+        assert index >= 0
+        w = w.replace('+', '')
+        if w in byru:
+            print('WARNING, addendum overwites corpus item (or duplicate addendum word)! Fix corpus instead:', w)
+            continue
+        w = w[:index+1] + '\u0301' + w[index+1:]
+        byru[w.replace('\u0301', '')].update([w])
 
     dedup = []
     for ru in byru.keys():
